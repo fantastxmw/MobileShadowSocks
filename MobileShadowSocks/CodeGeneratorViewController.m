ca1b93807e8a5a6caa5c968a3d51d39cf14e9099
Linus Yang <laokongzi@gmail.com>
2014-01-26 14:24:27 +0800

    Add social sharing feature

--- MobileShadowSocks/CodeGeneratorViewController.m
+++ MobileShadowSocks/CodeGeneratorViewController.m
@@ -8,9 +8,22 @@
 
 #import "CodeGeneratorViewController.h"
 
+#ifdef __IPHONE_6_0
+#import <Social/Social.h>
+#endif
+
 #define kCodeSize 256.0f
 
-@interface CodeGeneratorViewController ()
+typedef enum {
+    ShareActionCopy = 0,
+    ShareActionPhoto,
+    ShareActionTwitter,
+    ShareActionWeibo,
+    
+    ShareActionCount
+} ShareAction;
+
+@interface CodeGeneratorViewController () <UIActionSheetDelegate>
 
 @property (nonatomic, retain) NSString *codeLink;
 @property (nonatomic, retain) UIImageView *codeImageView;
@@ -18,6 +31,8 @@
 @end
 
 @implementation CodeGeneratorViewController
+
+#pragma mark - View life cycle
 
 - (id)initWithQRCodeLink:(NSString *)link
 {
@@ -66,6 +81,28 @@
     [super dealloc];
 }
 
+#pragma mark - Private methods
+
+- (void)showShareMessage:(NSString *)message
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Share QR Code", nil)
+                                                    message:message
+                                                   delegate:self
+                                          cancelButtonTitle:NSLocalizedString(@"OK",nil)
+                                          otherButtonTitles:nil];
+    [alert show];
+    [alert release];
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *) contextInfo;
+{
+    if (error) {
+        [self showShareMessage:NSLocalizedString(@"Failed to save QR code image.", nil)];
+    } else {
+        [self showShareMessage:NSLocalizedString(@"Image is saved to photo library.", nil)];
+    }
+}
+
 - (void)generateQRCode
 {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
@@ -83,7 +120,92 @@
 
 - (void)shareCodeImage
 {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:NSLocalizedString(@"Share QR Code",nil)
+                                                             delegate:self
+                                                    cancelButtonTitle:NSLocalizedString(@"Cancel",nil)
+                                               destructiveButtonTitle:nil
+                                                    otherButtonTitles:
+                                  NSLocalizedString(@"Copy Link", nil),
+                                  NSLocalizedString(@"Save to Photo Library", nil),
+                                  NSLocalizedString(@"Share to Twitter", nil),
+                                  NSLocalizedString(@"Share to Weibo", nil),
+                                  nil];
+    [actionSheet showInView:self.view];
+    [actionSheet release];
+}
+
+#pragma mark - UIActionSheet Delegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    BOOL isTwitterAvailable;
+    BOOL isWeiboAvailable;
     
+    isTwitterAvailable = NO;
+    isWeiboAvailable = NO;
+#ifdef __IPHONE_6_0
+    if ([SLComposeViewController class]) {
+        if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+            isTwitterAvailable = YES;
+        }
+        if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeSinaWeibo]) {
+            isWeiboAvailable = YES;
+        }
+    }
+#endif
+    
+    ShareAction action = (ShareAction) buttonIndex;
+    switch (action) {
+        case ShareActionCopy: {
+            UIPasteboard *pasteBoard = [UIPasteboard generalPasteboard];
+            pasteBoard.string = self.codeLink;
+            NSString *message = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"Link is copied to pasteboard", nil), self.codeLink];
+            [self showShareMessage:message];
+            break;
+        }
+            
+        case ShareActionPhoto: {
+            if (self.codeImageView.image) {
+                UIImageWriteToSavedPhotosAlbum(self.codeImageView.image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+            }
+            break;
+        }
+            
+        case ShareActionTwitter: {
+            if (isTwitterAvailable) {
+                SLComposeViewController *composeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+                if (composeViewController) {
+                    if (self.codeImageView.image) {
+                        [composeViewController addImage:self.codeImageView.image];
+                    }
+                    [composeViewController setInitialText:NSLocalizedString(@"I've shared a #ShadowSocks profile with QR Code.", nil)];
+                    [self presentViewController:composeViewController animated:YES completion:nil];
+                }
+            } else {
+                [self showShareMessage:NSLocalizedString(@"Twitter account is not available.", nil)];
+            }
+            break;
+        }
+            
+        case ShareActionWeibo: {
+            if (isWeiboAvailable) {
+                SLComposeViewController *composeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeSinaWeibo];
+                if (composeViewController) {
+                    if (self.codeImageView.image) {
+                        [composeViewController addImage:self.codeImageView.image];
+                    }
+                    [composeViewController setInitialText:NSLocalizedString(@"I've shared a #ShadowSocks# profile with QR Code.", nil)];
+                    [self presentViewController:composeViewController animated:YES completion:nil];
+                }
+            } else {
+                [self showShareMessage:NSLocalizedString(@"Sina Weibo account is not available.", nil)];
+            }
+            break;
+        }
+            
+        default:
+            break;
+    }
 }
 
 @end
